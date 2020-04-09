ffbb738f89652b6f1d19c9805fd4e7e1795bddb8
Sangriale <senora.pelos@gmail.com>
2020-04-09 14:42:50 -0500

    add switch case to liri

--- liri.js
+++ liri.js
@@ -1,1 +1,34 @@
 require("dotenv").config();
+// Grab Spotify keys securely 
+var keys = require("./keys.js");
+
+
+// Grab the axios package
+var axios = require("axios");
+
+// Grab Spotify package
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var command = process.argv [2];
+// Use slice and join in case artist has spaces in name
+var input = process.argv.slice(3).join(" ");
+console.log (command, input);
+
+switch (command) {
+    case "concert-this":
+        concert();
+        break;
+    case "spotify-this-song":
+        music();
+        break;
+    case "movie-this":
+        movie();
+        break;
+    case "do-what-it-says":
+        random();
+        break;
+    default:
+        console.log("Please submit a valid request");
+    
+}
