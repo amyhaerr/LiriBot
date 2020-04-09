53292dfde18ffdbb99cb91397e523a1703985ac2
Sangriale <senora.pelos@gmail.com>
2020-04-09 15:29:43 -0500

    set up switch command

--- liri.js
+++ liri.js
@@ -6,14 +6,19 @@ var keys = require("./keys.js");
 // Grab the axios package
 var axios = require("axios");
 
+var moment = require("moment");
+
+// grab and read random.txt
+var fs = require("fs");
+
 // Grab Spotify package
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-var command = process.argv [2];
+var command = process.argv[2];
 // Use slice and join in case artist has spaces in name
 var input = process.argv.slice(3).join(" ");
-console.log (command, input);
+console.log(command, input);
 
 switch (command) {
     case "concert-this":
@@ -30,5 +35,14 @@ switch (command) {
         break;
     default:
         console.log("Please submit a valid request");
-    
+
+}
+
+function concert() {
+    var concertUrl = `https://rest.bandsintown.com/artists/${input}events?app_id=codingbootcamp`;
+
+    axios
+        .get(concertUrl).then(function (repsonse) {
+            console.log(response.data);
+        });
 }
