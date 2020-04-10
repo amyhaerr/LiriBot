41ab85cf751610e1079b8572337ec189a23521b4
Sangriale <senora.pelos@gmail.com>
2020-04-10 11:05:51 -0500

    got venue information for artist and spotify for artist

--- liri.js
+++ liri.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 // Grab Spotify keys securely 
 var keys = require("./keys.js");
 
-
 // Grab the axios package
 var axios = require("axios");
 
-var moment = require("moment");
-
 // grab and read random.txt
 var fs = require("fs");
+
+var moment = require("moment");
 
 // Grab Spotify package
 var Spotify = require('node-spotify-api');
@@ -39,14 +38,28 @@ switch (command) {
 }
 
 function concert() {
-    var concertUrl = `https://rest.bandsintown.com/artists/${input}events?app_id=codingbootcamp`;
+    var concertURL = `https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`;
 
-    axios
-        .get(concertUrl).then(function (repsonse) {
-            
+    axios.get(concertURL).then(function (response) {
         
-            for (var i=0 ; i < response.length; i++) {
+        // Venue Information
+        for (var i = 0; i < response.data.length; i++) {
+            console.log (`\nVenue Name: ${response.data[i].venue.name}\nVenue Location: ${response.data[i].venue.city}, ${response.data[i].venue.country},\nEvent date and time: ${moment(response.data[i].datetime).format("MM-DD-YYYY LT")}`);
+        };
+    });
+}
 
-            }
-        });
+function music() {
+    if (command === "spotify-this-song" && process.argv[3] === undefined) {
+        input = "Chupando";
+    }
+
+    spotify
+    .search ({ type: 'track', query: input, limit: 1 }, function(err, data){
+        if (err) {
+            console.log(`an error occured: ${err}`);
+        } else {
+            console.log(`\nArtist(s): ${data.tracks.items[0].artists[0].name}\nSong Name: ${data.tracks.items[0].name}\nPreview Link: ${data.tracks.items[0].preview_url}\nAlbum: ${data.tracks.items[0].album.name}`);
+        };
+    });
 }
